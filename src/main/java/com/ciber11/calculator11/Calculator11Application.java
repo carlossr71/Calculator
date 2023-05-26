6c043dce868b10194306f6d398f741fa1e5296da
ciber11 <carlossr71@educastur.es>
2023-05-26 12:25:51 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber11/calculator11/Calculator11Application.java
@@ -0,0 +1,13 @@
+package com.ciber11.calculator11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator11Application.class, args);
+	}
+
+}
